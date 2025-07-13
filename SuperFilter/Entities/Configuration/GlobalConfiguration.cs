a864aea8a616101ac70fd36f112c9f4a3317bcdb
Ibramadi75 <ibrahim75madi@gmail.com>
2025-07-13 14:58:28 +0200

    Refactor GlobalConfiguration to use required properties for HasFilters and HasSorts; remove unused StandardException

--- SuperFilter/Entities/Configuration/GlobalConfiguration.cs
+++ SuperFilter/Entities/Configuration/GlobalConfiguration.cs
@@ -4,7 +4,6 @@ public class GlobalConfiguration
 {
     public Dictionary<string, FieldConfiguration> PropertyMappings { get; set; } = new();
     public OnErrorStrategy MissingOnStrategy { get; set; } = OnErrorStrategy.ThrowException;
-    public Exception StandardException { get; set; } = new SuperfilterException();
-    public IHasFilters HasFilters { get; set; }
-    public IHasSorts HasSorts { get; set; }
+    public required IHasFilters HasFilters { get; init; }
+    public required IHasSorts HasSorts { get; init; }
 }
--- SuperFilter/ExpressionBuilders/Primary/BoolExpressionBuilder.cs
+++ SuperFilter/ExpressionBuilders/Primary/BoolExpressionBuilder.cs
@@ -1,5 +1,4 @@
 using System.Linq.Expressions;
-using Superfilter.Constants;
 
 namespace Superfilter.ExpressionBuilders;
 
